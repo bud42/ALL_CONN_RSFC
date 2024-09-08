388246dd90b4f1e6c97fde78854ade8d3eaa10e6
bud42 <bdboyd42@gmail.com>
2024-09-08 15:10:19 -0400

    add single_subject_roi_only

---
+++ src/single_subject_roi_only/run.sh
@@ -0,0 +1,8 @@
+echo "Running CONN Pipeline"
+xvfb-run \
+-e /OUTPUTS/xvfb.err -f /OUTPUTS/xvfb.auth \
+-a --server-args "-screen 0 1600x1200x24" \
+/opt/conn/run_conn.sh /opt/mcr/v912 batch /opt/src/single_subject_roi_only/main.m
+rm /OUTPUTS/xvfb.auth /OUTPUTS/xvfb.err
+
+echo "ALL DONE!"
